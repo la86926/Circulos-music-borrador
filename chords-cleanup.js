b7053a0f3b901c2026bf24f110dad8f4feb63444
la86926 <la86926@gmail.com>
2026-08-02 21:09:19 -0500

    Actualizar carga de correcciones visuales

--- chords-cleanup.js
+++ chords-cleanup.js
@@ -1,6 +1,6 @@
 (()=>{
 'use strict';
-Promise.all([import('./performance-mode.js?v=7'),import('./menu-fixes.js?v=2')]).catch(()=>{});
+Promise.all([import('./performance-mode.js?v=7'),import('./menu-fixes.js?v=3')]).catch(()=>{});
 const PC={C:0,'C#':1,Db:1,D:2,'D#':3,Eb:3,E:4,F:5,'F#':6,Gb:6,G:7,'G#':8,Ab:8,A:9,'A#':10,Bb:10,B:11};
 const CHROMATIC=['C','C#','D','D#','E','F','F#','G','G#','A','A#','B'];
 const LATIN={C:'DO',D:'RE',E:'MI',F:'FA',G:'SOL',A:'LA',B:'SI'};
